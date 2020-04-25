a0b137fb08dba4efbb6a47a7f5f0f5089012f193
Tshiamo Todd <tshiamo.mokwena19@gmail.com>
2020-04-25 16:54:04 +0200

    Updates

---
+++ precache-manifest.6c45cddd27ecee5f441bb69b2c8d77a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4d7ef09ba578129b21bf3cf0e09bc3",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "1082d6ab6e1f2a45504a",
+    "url": "/covid-tracker/static/css/main.7ba5f153.chunk.css"
+  },
+  {
+    "revision": "ee52f2df8900750a8136",
+    "url": "/covid-tracker/static/js/2.faee2869.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.faee2869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1082d6ab6e1f2a45504a",
+    "url": "/covid-tracker/static/js/main.74c9d56d.chunk.js"
+  },
+  {
+    "revision": "b19916e0cf1f97af8115",
+    "url": "/covid-tracker/static/js/runtime-main.45daefb2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
